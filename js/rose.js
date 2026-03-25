23668095dfe5c67d36a6c3e44515976887f9b3ee
Bluejazzyrose <jasmine.fryefamily@gmail.com>
2026-03-25 11:10:38 -0500

    Added a footer and started working on nav layout

---
+++ js/rose.js
@@ -0,0 +1,10 @@
+function updateHeaderHeight() {
+    const header = document.querySelector("header");
+    document.documentElement.style.setProperty(
+        "--header-height",
+        header.offsetHeight + "px"
+    );
+}
+
+window.addEventListener("load", updateHeaderHeight);
+window.addEventListener("resize", updateHeaderHeight);
